4bee8be7dd6d7196fa3ba07755e5ffc63594a814
tishetiho <moteoAZ@yandex.ru>
2026-03-27 10:46:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = "7938896805:AAGx7neVzOUXbzl8wSIsSZ93dB4ZxAWhldo"
-BOT_USERNAME = "prosnylsa_bot"
-ADMIN_IDS = [int(id) for d in os.getenv("ADMIN_IDS", "5078764886").split(",")]
+BOT_TOKEN = "8628542460:AAG9x0N14OQlqSnhHswzaRSqkd7lo5TdYHY"
+BOT_USERNAME = "MellGift_robot"
+ADMIN_IDS = [int(id) for d in os.getenv("5078764886").split(",")]
 DATABASE_PATH = os.getenv("DATABASE_PATH", "database.db")
