8ffb8f59e36f62d1bac2dc27fa65e1df6614849b
tishetiho <moteoAZ@yandex.ru>
2026-03-27 10:51:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 BOT_TOKEN = "8628542460:AAG9x0N14OQlqSnhHswzaRSqkd7lo5TdYHY"
-BOT_USERNAME = "MellGift_robot"
+BOT_USERNAME = "MellGifts_robot"
 ADMIN_IDS = [int(id) for d in os.getenv("ADMIN_IDS", "5078764886").split(",")]
 DATABASE_PATH = os.getenv("DATABASE_PATH", "database.db")
